63186d22b4df17e4a9a3127ee91619762ad8d82e
tmontanaro <teodoro.montanaro@unisalento.it>
2023-04-15 00:48:05 +0200

    Working project

--- serverApp.js
+++ serverApp.js
@@ -1,4 +1,7 @@
 var express = require("express");
+const MongoClient = require('mongodb').MongoClient;
+const uri = 'mongodb://130.192.137.1/TemperatureDB';
+
 var app = express();
 app.listen(3000, () => {
     console.log("Server running on port 3000");
@@ -10,6 +13,36 @@ app.use(
 )
 app.use(express.json());
 app.post("/temperature", (req, res, next) => {
-    console.log(req.body.temperature);
+    var temperature = req.body.temperature;
+    var timestamp = req.body.timestamp;
+    var sensor = req.body.sensor;
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+    async function run() {
+        try {
+
+            await client.connect();
+
+            const database = client.db("TemperatureDB");
+            const temperatureColl = database.collection("temperature");
+            // create a document to be inserted
+            const doc = {
+                value: temperature,
+                timestamp: timestamp,
+                sensorId: sensor,
+                roomId: 'room1'
+            };
+            const result = await temperatureColl.insertOne(doc);
+            console.log(
+                `Document inserted with the _id: ${result.insertedId}`,
+            );
+
+        } finally {
+            await client.close();
+        }
+
+    }
+
+    run().catch(console.dir);
     res.sendStatus(200);
 });
